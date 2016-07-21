a0325c52372eb27c37513e10ddcea0d34d9612de
张卓 <contrary.zhuo@gmail.com>
2016-07-21 23:43:55 +0800

    [init] page: home

---
+++ app/src/pages/Home.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>hello</h1>
+</template>
+
+<script>
+export default {
+}
+</script>
--- app/src/routes.js
+++ app/src/routes.js
@@ -1,6 +1,8 @@
-import Vue from 'vue'
+import Home from './pages/Home.vue'
 
 export default {
   '/': {
+    component: Home,
+    name: Home
   }
 }
