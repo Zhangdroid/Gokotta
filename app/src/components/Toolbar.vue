0dfe24acd7f02284367c015901905e29932bad95
张卓 <contrary.zhuo@gmail.com>
2016-07-23 22:47:37 +0800

    [init] component: toolbar

---
+++ app/src/components/Toolbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="toolbar"></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.toolbar {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 8vh;
+  background-color: rgba(#009688, .8);
+  backdrop-filter: blur(5px);
+}
+</style>
