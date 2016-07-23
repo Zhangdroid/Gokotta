73ba65991d3b767ced489218e85edc0588f634d3
张卓 <contrary.zhuo@gmail.com>
2016-07-23 22:49:23 +0800

    [update] add components and style

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,15 +1,31 @@
-<style>
-</style>
-
 <template>
-  <div>
+  <div class="app">
+    <toolbar></toolbar>
+    <sidebar></sidebar>
     <router-view></router-view>
+    <player></player>
   </div>
 </template>
 
 <script>
-  import store from 'src/vuex/store'
-  export default {
-    store
+import store from './vuex/store'
+
+import toolbar from './components/Toolbar.vue'
+import sidebar from './components/Sidebar.vue'
+import player from './components/Player.vue'
+export default {
+  store,
+  components: {
+    toolbar,
+    sidebar,
+    player
   }
+}
 </script>
+
+<style src="./styles/main.scss"></style>
+<style lang="scss" scoped>
+.app {
+  margin: 0 18vw;
+}
+</style>
