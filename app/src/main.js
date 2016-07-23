19f70a8509dc37b2dc50fd190cd2179690dd4a68
张卓 <contrary.zhuo@gmail.com>
2016-07-23 22:43:58 +0800

    [delete] init database when use it instead of at start

--- app/src/main.js
+++ app/src/main.js
@@ -4,12 +4,6 @@ import Router from 'vue-router'
 import App from './App'
 import routes from './routes'
 
-import { openDB } from './services/database'
-
-(async () => {
-  Vue.prototype.$db = await openDB()
-})()
-
 Vue.use(Router)
 Vue.config.debug = true
 
