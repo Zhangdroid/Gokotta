2fb82594b533010f054294bc33d50ef94a50b6d9
张卓 <contrary.zhuo@gmail.com>
2016-07-23 22:47:21 +0800

    [init] component: sidebar

---
+++ app/src/components/Sidebar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="sidebar"></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.sidebar {
+  position: fixed;
+  top: 8vh;
+  bottom: 18vh;
+  left: 0;
+  width: 18vw;
+  background-color: #fff;
+}
+</style>
