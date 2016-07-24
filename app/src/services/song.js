fe77223aed3a148b4a8615265aaa1eb70d953097
张卓 <contrary.zhuo@gmail.com>
2016-07-24 17:45:19 +0800

    [change] encode song's cover before into database

--- app/src/services/song.js
+++ app/src/services/song.js
@@ -1,8 +1,17 @@
 import GUID from '../utils/guid'
+import encode from '../utils/encode'
 
 class Song {
   constructor (tag, path) {
     this.id = GUID()
+    for (let prop in tag) {
+      if (prop === 'picture') {
+        if (tag.picture[0].data) {
+          tag.cover = `data:image/png;base64,${encode(tag.picture[0].data)}`
+          delete tag.picture
+        }
+      }
+    }
     Object.assign(this, tag, { path })
   }
 }
