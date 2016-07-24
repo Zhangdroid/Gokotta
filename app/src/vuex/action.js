7171236767c1ffc2a3075fd0344189aa700149ed
张卓 <contrary.zhuo@gmail.com>
2016-07-24 17:48:20 +0800

    [update] vuex: new action - initDatabase

--- app/src/vuex/action.js
+++ app/src/vuex/action.js
@@ -1,5 +1,14 @@
 import * as types from './mutation-types'
 
+
+import { openDB } from '../services/database'
+
 export const playSong = ({ dispatch, state }, song) => {
   dispatch(types.PLAY_SONG, song)
+}
+
+export const initDatabase = ({ dispatch, state }) => {
+  openDB().then(database => {
+    dispatch(types.INIT_DATABASE, database)
+  })
 }
