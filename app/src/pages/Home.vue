c1e0d27f9de94e81828e0664143297c2d9b4752c
张卓 <contrary.zhuo@gmail.com>
2016-07-24 17:47:13 +0800

    [update]: update styles

--- app/src/pages/Home.vue
+++ app/src/pages/Home.vue
@@ -1,12 +1,31 @@
 <template>
-  <file-selector></file-selector>
+  <div class="home">
+    <file-selector></file-selector>
+    <play-list></play-list>
+  </div>
 </template>
 
 <script>
 import FileSelector from '../components/FileSelector.vue'
+import PlayList from '../components/PlayList.vue'
 export default {
   components: {
-    FileSelector
+    FileSelector,
+    PlayList
   }
 }
 </script>
+
+<style lang="scss" scoped>
+@import "../styles/variables";
+.home {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: $sidebar-width;
+  right: 0;
+  padding-top: $toolbar-height;
+  padding-bottom: $player-height;
+  overflow-y: scroll;
+}
+</style>
